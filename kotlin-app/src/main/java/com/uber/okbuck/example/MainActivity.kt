16d4b92b6cadcaa7c1a3ddc8f8bfa98e1e406e27
Rifqi M Fahmi <rifqi.fahmi@tokopedia.com>
2019-06-29 10:50:56 +0700

    Test coroutine

--- kotlin-app/src/main/java/com/uber/okbuck/example/MainActivity.kt
+++ kotlin-app/src/main/java/com/uber/okbuck/example/MainActivity.kt
@@ -1,10 +1,16 @@
 package com.uber.okbuck.example
 
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
 import android.widget.Toast
 import com.uber.okbuck.example.sqldelightmodel.GithubRepo
 import dagger.android.support.DaggerAppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 class MainActivity : DaggerAppCompatActivity() {
@@ -23,6 +29,18 @@ class MainActivity : DaggerAppCompatActivity() {
 
         val repo = GithubRepo.create(100, "OkBuck", "auto buck")
         Toast.makeText(this, repo.name + ": " + repo.description, Toast.LENGTH_SHORT).show()
+
+        testCoroutine()
+    }
+
+    private fun testCoroutine() {
+        val btn = findViewById<Button>(R.id.coroutineBtn)
+        btn.setOnClickListener {
+            GlobalScope.launch(Dispatchers.Main) {
+                delay(1000)
+                Log.d("TEST_COROUTINE", "YEAY")
+            }
+        }
     }
 
     companion object {
